a950bc417c4c71d8b95beea006fbb6df28626467
Federico Abreu Seymour <89706680+Fed-Abreu@users.noreply.github.com>
2021-09-05 17:33:51 -0400

    Update Solution04.java

--- src/main/java/Solution04.java
+++ src/main/java/Solution04.java
@@ -20,13 +20,13 @@ public class Solution04 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a noun: ");
-        String noun = input.nextLine();
+            String noun = input.nextLine();
         System.out.print("Enter a verb: ");
-        String verb = input.nextLine();
+            String verb = input.nextLine();
         System.out.print("Enter an adjective: ");
-        String adjective = input.nextLine();
+            String adjective = input.nextLine();
         System.out.print("Enter an adverb: ");
-        String adverb = input.nextLine();
+            String adverb = input.nextLine();
         System.out.printf("Do you %s your %s %s %s? That's hilarious!", verb, adjective, noun, adverb);
 
 
